f46f4c37d541759143e55e36f3dc6554f4c3ca95
usuario_tarde <usuario_tarde@DESKTOP-3LHBG1B>
2025-11-25 17:43:27 +0100

    subelooooo sentadilla

---
+++ sentadilla/src/sentadilla_deculo/sentadadeculo.java
@@ -0,0 +1,24 @@
+package sentadilla_deculo;
+
+import java.util.Scanner;
+
+public class sentadadeculo {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	
+	
+	int num1;
+	int num2;
+	
+	System.out.println("Escribe el primer numero");
+	num1=sc.nextInt();
+	
+	System.out.println("Escribe el segundo numero");
+	num2=sc.nextInt();
+	
+	int resultado=num1-num2;
+	
+	System.out.println(resultado);
+	sc.close();}
+}
+
